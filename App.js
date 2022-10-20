595ed61afeba9e355384cae2619766515abecb2c
metalcupcake5 <imcoolcom5@gmail.com>
2022-10-20 01:25:46 -0700

    feat: automatically render new messages

--- App.js
+++ App.js
@@ -11,36 +11,6 @@ import React, { useState, useEffect, useRef } from "react";
 
 let ws = new WebSocket("ws://192.168.29.91:8080");
 let interval;
-const messages = [];
-function connect(socket) {
-    ws.onopen = () => {
-        console.log("connection established");
-        ws.send("message from phone");
-        console.log("sent message");
-        clearInterval(interval);
-    };
-    ws.onclose = (e) => {
-        // connection closed
-        console.log("connection closed");
-        if (!interval) {
-            console.log("no interval");
-            interval = setInterval(() => {
-                console.log("attempting to reconnect");
-                ws = new WebSocket("ws://192.168.29.91:8080");
-                connect(ws);
-            }, 1000 * 5);
-        }
-    };
-    ws.onmessage = (e) => {
-        // a message was received
-        console.log(`received: ${e.data}`);
-        messages.push({
-            id: Math.random().toString(12).substring(0),
-            message: e.data,
-            prefix: "recieved: "
-        });
-    };
-}
 
 const Item = ({ title }) => (
     <View style={styles.item}>
@@ -49,20 +19,43 @@ const Item = ({ title }) => (
 );
 
 export default function App() {
-    connect(ws);
-
     let [text, setText] = useState("");
+    let [messages, setMessages] = useState([]);
     const inputRef = useRef();
 
-    console.log(messages);
+    function connect(socket) {
+        ws.onopen = () => {
+            console.log("connection established");
+            ws.send("message from phone");
+            console.log("sent message");
+            clearInterval(interval);
+        };
+        ws.onclose = (e) => {
+            // connection closed
+            console.log("connection closed");
+            if (!interval) {
+                console.log("no interval");
+                interval = setInterval(() => {
+                    console.log("attempting to reconnect");
+                    ws = new WebSocket("ws://192.168.29.91:8080");
+                    connect(ws);
+                }, 1000 * 5);
+            }
+        };
+        ws.onmessage = (e) => {
+            // a message was received
+            console.log(`received: ${e.data}`);
+            setMessages([...messages, {
+                id: Math.random().toString(12).substring(0),
+                message: e.data,
+                prefix: "recieved: "
+            }]);
+        };
+    }
+    connect(ws);
 
     const renderItem = ({ item }) => <Item title={item.prefix + item.message} />;
-    const DATA = [
-        {
-            id: Math.random().toString(12).substring(2),
-            message: "hi",
-        },
-    ];
+
     return (
         <View style={styles.container}>
             <View style={styles.title}>
