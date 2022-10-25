df00546ef4cb6926a18ab8e73f8d339c49d67d15
metalcupcake5 <imcoolcom5@gmail.com>
2022-10-25 01:23:07 -0700

    feat: fix + styles

--- elements/Chat.js
+++ elements/Chat.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import Item from "./Item";
 
 const renderItem = ({ item }) => <Item title={item.prefix + item.message} />;
--- elements/Item.js
+++ elements/Item.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default Item = ({ title }) => (
     <View style={styles.item}>
         <Text style={styles.title1}>{title}</Text>
     </View>
 );
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: "#f9c2ff",
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title1: {
+        fontSize: 16,
+    },
+});
