4fcdc436e9d3117c5ee64b28eeaaba90378dbd80
metalcupcake5 <imcoolcom5@gmail.com>
2022-10-31 20:13:42 -0700

    fix: idk what i did

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -48,6 +48,7 @@ export default ({ navigation }) => {
                 <Button
                     title="send"
                     onPress={() => {
+                        if(text.length < 1) return;
                         setMessages([
                             ...messages,
                             {
@@ -62,8 +63,8 @@ export default ({ navigation }) => {
                             id: global.serverId ?? -1,
                         };
                         global.ws.send(JSON.stringify(message));
-                        text = "";
                         setText("");
+                        inputRef.current.setNativeProps({ text: "" });
                         inputRef.current.blur();
                         console.log("sent");
                     }}
